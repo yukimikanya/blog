eefeaafcf8f030436ace73a2bb197369aae3875e
yukimikanya <ann.yukiworks@gmail.com>
2023-02-25 02:38:32 +0900

    ブログの更新です

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,13 @@
 import '@/styles/globals.css';
+import { createLogger } from '@vercel/analytics';
 
 function MyApp({ Component, pageProps }) {
+  const logger = createLogger({
+    // オプションを指定することができます
+  });
+
+  logger.page();
+
   return (
     <div className="font-quicksand">
       <Component {...pageProps} />
