60c7a3b5e9af1755f6218a8ef823574df76704fa
yukimikanya <ann.yukiworks@gmail.com>
2023-02-25 03:57:48 +0900

    1

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,11 @@
 import '@/styles/globals.css';
+import { Analytics } from '@vercel/analytics/react';
 
 function MyApp({ Component, pageProps }) {
   return (
     <div className="font-quicksand">
       <Component {...pageProps} />
+      <Analytics />
     </div>
   );
 }
